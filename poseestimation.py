424b8a0125c70cbd8acec85dde538f170021d0d1
Kaniz Fatima <liza_5757@yahoo.com>
2022-03-23 15:03:22 +0600

    Add files via upload

--- poseestimation.py
+++ poseestimation.py
@@ -18,7 +18,7 @@ pose = mp_pose.Pose(
 
 # create capture object
 cap = cv2.VideoCapture('pixel.mp4')  # 'video.mp4'
-
+m = 0
 
 
 while cap.isOpened():
@@ -31,24 +31,36 @@ while cap.isOpened():
 
         # process the RGB frame to get the result
         results = pose.process(RGB)
-        #cars[0] = ""
+
         # Perform pose detection after converting the image into RGB format.
         #results = pose.process(cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB))
-
+        thisxlist = []
+        thisylist = []
         # Check if any landmarks are found.
         if results.pose_landmarks:
 
             # Iterate two times as we only want to display first two landmarks.
             for i in range(33):
                 # Display the found normalized         landmarks.
-                print(
-                    f'{mp_pose.PoseLandmark(i).name}:\n{results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value]}')
-                #cars.append(results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value])
+                print(f'{mp_pose.PoseLandmark(i).name}:\n{results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value]}')
+
+                thisxlist.append(results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x)
+                thisylist.append(results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y)
+
+                # thislist.append(f'x: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x * image_width}')
+                # thislist.append(f'y: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y * image_height}')
+
+
+                # print(f'x: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x * image_width}')
+                # print(f'y: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y * image_height}')
+
+        maxx = max(thisxlist)
+        maxy = max(thisylist)
+        minx = min(thisxlist)
+        miny = min(thisylist)
 
 
 
-
-       #print(cars);
 
         #print(results.pose_landmarks)
         # draw detected skeleton on the frame
@@ -59,6 +71,11 @@ while cap.isOpened():
         # show the final output'
 
         frame = cv2.resize(frame, (1600, 900))
+        cv2.imwrite('F:/poseestimation/crop/Frame' + str(m) + '.jpg', frame)
+        img = cv2.imread('F:/poseestimation/crop/Frame' + str(m) + '.jpg')
+        cropped_image = img[80:280, 150:330] # maxx, maxy, minx, miny
+        cv2.imwrite("F:/poseestimation/crop/CroppedImage"+ str(m) + ".jpg", cropped_image)
+        m += 1
         cv2.imshow('Output', frame)
     except:
         break
